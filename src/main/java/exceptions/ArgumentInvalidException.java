ceeff5b87d7c8acdbbb6873721b615e203d37c7a
Mạnh Nguyễn <manhnd.jin@gmail.com>
2023-08-31 17:27:44 +0700

    ci: add secret token

--- src/main/java/exceptions/ArgumentInvalidException.java
+++ src/main/java/exceptions/ArgumentInvalidException.java
@@ -4,8 +4,8 @@ import lombok.Getter;
 
 @Getter
 public class ArgumentInvalidException extends RuntimeException {
-    private String message;
-    private final Integer code = ExceptionCode.BAD_REQUEST;
+    private final String message;
+    private static final Integer code = ExceptionCode.BAD_REQUEST;
 
     public ArgumentInvalidException(String message) {
         this.message = message;
