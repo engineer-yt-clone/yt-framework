4bac10a3799651a01bc9c561094c68dca25dd5eb
Mạnh Nguyễn <manhnd.jin@gmail.com>
2023-09-18 22:01:34 +0700

    fix: update inteceptor

--- src/main/java/com/engineer/yt/tracing/CorrelationInterceptor.java
+++ src/main/java/com/engineer/yt/tracing/CorrelationInterceptor.java
@@ -2,8 +2,7 @@ package com.engineer.yt.tracing;
 
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import lombok.extern.slf4j.Slf4j;
-import org.slf4j.MDC;
+
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerInterceptor;
 
@@ -11,7 +10,7 @@ import java.util.UUID;
 import org.springframework.web.servlet.ModelAndView;
 
 @Component
-@Slf4j
+
 public class CorrelationInterceptor implements HandlerInterceptor {
     private static final String CORRELATION_ID_HEADER = "X-Correlation-ID";
     private static final String CORRELATION_ID_LOG_VAR_NAME = "correlationId";
@@ -20,14 +19,13 @@ public class CorrelationInterceptor implements HandlerInterceptor {
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
         System.out.println();
         final String correlationId = getOrGenerateCorrelationId(request, response);
-        MDC.put(CORRELATION_ID_LOG_VAR_NAME, correlationId);
-        log.info("CorrelationId : {}", correlationId);
+
         return true;
     }
 
     @Override
     public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
-        MDC.remove(CORRELATION_ID_LOG_VAR_NAME);
+
     }
 
     @Override
